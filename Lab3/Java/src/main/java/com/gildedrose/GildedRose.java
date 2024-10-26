21a497815ff2d8547233ba7f827772d63ebc091a
Latko Artem <latko8973@gmail.com>
2024-10-26 15:27:37 +0300

    Elimination of redundant nested conditions I removed some nested conditionals to improve code readability. That is, I replaced it with 'else if'. For example, I separated the handling of cases when 'sellIn < 0' into separate 'else if' blocks at the end of the loop.

--- Lab3/Java/src/main/java/com/gildedrose/GildedRose.java
+++ Lab3/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,40 +11,38 @@ class GildedRose {
         for (Item item : items) {
             if (item.getName().equals("Aged Brie")
                 || item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.getQuality() < 50) {
-                        item.increaseQualituByOne();
+                if (item.getQuality() < 50) {
+                    item.increaseQualituByOne();
 
-                        if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.getSellIn() < 11) {
-                                item.increaseQualituByOne();
-                            }
+                    if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.getSellIn() < 11) {
+                            item.increaseQualituByOne();
+                        }
 
-                            if (item.getSellIn() < 6) {
-                                item.increaseQualituByOne();
-                            }
+                        if (item.getSellIn() < 6) {
+                            item.increaseQualituByOne();
                         }
                     }
-                } else {
-                if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                    item.decreaseQualituByOne();
                 }
+            } else if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                item.decreaseQualituByOne();
             }
 
             if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
                 item.setSellIn(item.getSellIn() - 1);
             }
 
-            if (item.getSellIn() < 0) {
-                if (item.getName().equals("Aged Brie")) {
+            if (item.getName().equals("Aged Brie")) {
+                if (item.getSellIn() < 0) {
                     item.increaseQualituByOne();
-                } else {
-                    if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                            item.decreaseQualituByOne();
-                        }
-                    } else {
-                        item.setQuality(0);
-                    }
+                }
+            } else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.getSellIn() < 0) {
+                    item.setQuality(0);
+                }
+            } else if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.getSellIn() < 0) {
+                    item.decreaseQualituByOne();
                 }
             }
         }
